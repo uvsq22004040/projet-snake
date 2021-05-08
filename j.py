358308dfe707a89ced0d1541e49a52c12d0447f6
judith <judith.faucon@ens.uvsq.fr>
2021-05-08 12:07:18 +0200

    ancien snake

---
+++ j.py
@@ -0,0 +1,6 @@
+def f(el,e):
+    el[0] = 0
+    e +=1
+el, e = [2], 2
+f(el,e)
+print(el,e)
---
+++ qcm.py
@@ -0,0 +1,10 @@
+
+
+
+
+#autre question
+v1 = [2, [5, 3]]
+v2 = list(v1)
+v2[1][0] = 0
+v2[0] = 0
+print(v1)
